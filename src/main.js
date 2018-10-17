3a3e3c29ce5e7d6b884d1c055338dfa39c4738e8
UboyZz <31951971+UboyZz@users.noreply.github.com>
2018-10-17 12:41:44 +0800

    Revert "完成登入功能"

--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// 导入 element-ui
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-// 导入字体图标样式
-import './assets/fonts/iconfont.css'
-// 导入全局样式表
-import './assets/css/global.css'
-// 导入axios
-import axios from 'axios'
 
 Vue.config.productionTip = false
-// 安装 element-ui
-Vue.use(ElementUI)
-// 设置请求根路径
-axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
-// 配置 axios
-Vue.prototype.$http = axios
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
-// 导入登入组建
-import Login from '@/components/Login'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-const router = new Router({
-  routes: [{ path: '/', redirect: '/login' }, { path: '/login', component: Login }]
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
 })
-
-// 为路由对象，添加beforeEach 导航首位
-router.beforeEach((to, from, next) => {
-  // 如果用户访问登入也，直接放行，
-  if (to.path === '/login') return next()
-  // 从sessionStorage 中获取道 保存的token值
-  const tokenStr = window.sessionStorage.getItem('token')
-  //  如果没有token 强制跳转到登入面
-  if (!tokenStr) return next('/login')
-  next()
-})
-
-export default router
